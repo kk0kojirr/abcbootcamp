06da201f1a23b1984708abd8ea26eb578e73fdba
example <example@example.com>
2021-03-29 22:31:00 +0900

    add 027, 161 other

--- abc122b/src/main.rs
+++ abc122b/src/main.rs
@@ -4,6 +4,15 @@ fn main() {
     input! {
         s: Chars,
     }
-    
-    println!("{:?}", s);
+    let mut cnt = 0;
+    let mut ans = 0;
+    for c in s {
+        if c == 'A' || c == 'C' || c == 'G' || c == 'T' {
+            cnt += 1;
+            ans = std::cmp::max(cnt, ans);
+        } else {
+            cnt = 0;
+        }
+    }
+    println!("{}", ans);
 }
